d4309a9e70264b71576dfa07f106c226d775dbde
Adam Sagaev <spartan11625@gmail.com>
2025-02-08 15:49:07 +0100

    First Release

---
+++ chatims-backend/src/main/java/com/app/chatims/ChatimsApplication.java
@@ -0,0 +1,13 @@
+package com.app.chatims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatimsApplication.class, args);
+	}
+
+}
---
+++ chatims-backend/src/test/java/com/app/chatims/ChatimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.chatims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
