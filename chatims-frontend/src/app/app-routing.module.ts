6569c183271ff298dc5f353f9eee5c3c284f7fa8
Adam Sagaev <spartan11625@gmail.com>
2025-04-18 23:50:37 +0200

    Add back button in register.component to dashboard

--- chatims-frontend/src/app/app-routing.module.ts
+++ chatims-frontend/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SplashScreenComponent } from './shared/splash-screen/splash-screen.component';  
-import { ChatComponent } from './chat/chat.component';  
-import { RegisterComponent } from './user/register/register.component';
+import { ChatComponent } from './chat/chat.component'; 
 
 const routes: Routes = [
   { path: '', component: SplashScreenComponent },  
-  { path: 'chat', component: ChatComponent },   
-  { path: 'register', component: RegisterComponent },      
+  { path: 'chat', component: ChatComponent },     
 ];
 
 @NgModule({
--- chatims-frontend/src/app/user/register/register.component.ts
+++ chatims-frontend/src/app/user/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output  } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -16,14 +17,18 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule],
+    MatButtonModule,],
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  @Output() back = new EventEmitter<void>();
 
+  goBack() {
+    this.back.emit();
+  }
   registerForm: FormGroup;
   
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -48,4 +53,5 @@ export class RegisterComponent {
       );
     }
   }
+ 
 }
