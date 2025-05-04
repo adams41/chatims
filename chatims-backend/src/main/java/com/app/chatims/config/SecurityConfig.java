a9f8b916d99d436a3c6eb969498b7d5a84bc4ede
Adam Sagaev <spartan11625@gmail.com>
2025-05-04 20:26:56 +0200

    "Update SecurityConfig, application.properties and docker-compose.yaml"

--- chatims-backend/src/main/java/com/app/chatims/config/SecurityConfig.java
+++ chatims-backend/src/main/java/com/app/chatims/config/SecurityConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -15,8 +19,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsFilter()))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/users/register", "/h2-console/**").permitAll()
+                        .requestMatchers( "/users/register", "/h2-console/**").permitAll()
                         .requestMatchers("/chat").authenticated()
                         .anyRequest().authenticated()
                 )
@@ -34,8 +39,23 @@ public class SecurityConfig {
                         .clearAuthentication(true)
                         .deleteCookies("JSESSIONID")
                 )
-                .cors(Customizer.withDefaults());
+                ;
 
         return http.build();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
